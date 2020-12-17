295b056108f919b786aad29825c508a9e45ace80
Nicolas Franck <nicolas.franck@ugent.be>
2020-12-17 15:56:29 +0100

    configure legacy model Ugent::Log

--- app/models/ugent/log.rb
+++ app/models/ugent/log.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Purpose: previously a Plan could be removed, and we recorded that action in ugent_logs
+# Now a plan is merely deactivated, and so remains in the database
+
 module Ugent
 
   class Log < ApplicationRecord
