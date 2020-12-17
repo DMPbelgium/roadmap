8b2109bd619bd078cef697dc5ea654cff398f113
Nicolas Franck <nicolas.franck@ugent.be>
2020-12-17 14:24:40 +0100

    refactor to make rubocop happy

--- config/initializers/wicked_pdf_ugent.rb
+++ config/initializers/wicked_pdf_ugent.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 module DMPRoadmap
+
   class Application < Rails::Application
+
     WickedPdf.config = {
       exe_path: "#{ENV['GEM_HOME']}/bin/wkhtmltopdf"
     }
+
   end
+
 end
