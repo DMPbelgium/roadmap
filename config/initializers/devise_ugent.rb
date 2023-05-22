a8b25acc19fa0e1e38b213886f3e190490a1caf3
Nicolas Franck <nicolas.franck@ugent.be>
2023-05-22 16:13:43 +0200

    fix merge conflict: move local stuff to contact_us_local.rb

---
+++ config/initializers/devise_ugent.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+Devise.setup do |config|
+
+  config.mailer_sender = "info@dmponline.be"
+  OmniAuth.config.full_host = ENV["BASE_URL"]
+
+  config.omniauth(
+    :orcid,
+    ENV["ORCID_CLIENT_ID"],
+    ENV["ORCID_CLIENT_SECRET"], {
+      member: false,
+      sandbox: false }
+  )
+
+  config.omniauth(
+    :shibboleth, {
+      uid_field: ENV["SHIBBOLETH_UID_FIELD"],
+      fields: [],
+      extra_fields: %i[persistent-id eppn affiliation entitlement unscoped-affiliation targeted-id mail sn givenname department faculty] }
+  )
+
+end
--- config/initializers/ugent.rb
+++ config/initializers/ugent.rb
@@ -1,27 +1,392 @@
-Identifier.module_eval do
+# frozen_string_literal: true
+
+# apparently these modules are not loaded yet, so there functionality does not work yet
+require "user"
+require "identifier"
+require "org"
+require "users/omniauth_callbacks_controller"
+
+class Identifier
 
   def value_uniqueness_with_scheme
-    #override - start
-    #Org may have multiple login routes of the same type
-    if self.identifier_scheme.name == "shibboleth" && self.identifiable_type == "Org"
-      return true
-    end
-    #same orcid may be attached to several users
-    if self.identifier_scheme.name == "orcid" && self.identifiable_type == "User"
-      return true
-    end
-    #override - end
-    #old code
-    if Identifier.where(identifier_scheme: self.identifier_scheme,
-                        identifiable: self.identifiable).any?
+    # override - start
+    # Org may have multiple login routes of the same type
+    return true if identifier_scheme.name == "shibboleth" && identifiable_type == "Org"
+
+    # same orcid may be attached to several users
+    return true if identifier_scheme.name == "orcid" && identifiable_type == "User"
+
+    # override - end
+    # old code
+    if Identifier.where(identifier_scheme: identifier_scheme,
+                        identifiable: identifiable).any?
       errors.add(:identifier_scheme, _("already assigned a value"))
     end
   end
 
 end
 
-DMPRoadmap::Application.class_eval do
-  if File.exists?(Rails.root.join('config', 'custom.yml'))
-    config.custom = config_for(:custom)
+class User
+
+  def ensure_password
+    generate_password unless encrypted_password.present?
   end
+
+  # rubocop: disable Lint/UselessAssignment
+  def generate_password
+    password = Devise.friendly_token[0, 20]
+    password_confirmation = password
+  end
+  # rubocop: enable Lint/UselessAssignment
+
+  def guest?
+    org_id == Org.guest.id
+  end
+
+  def self.nemo
+    "n.n."
+  end
+
+  def nemo?
+    firstname.blank? || surname.blank? || firstname == User.nemo || surname == User.nemo
+  end
+
+  def alternative_accounts
+    scheme = IdentifierScheme.find_by_name("orcid")
+    orcid = identifiers.select { |id| id.identifier_scheme_id == scheme.id }.first
+
+    return [] if orcid.nil?
+
+    Identifier.where(
+      "identifier_scheme_id = ? AND identifiable_type = ? AND value = ? AND identifiable_id <> ?",
+      scheme.id,
+      "User",
+      orcid.value,
+      id
+    )
+              .map(&:identifiable)
+  end
+
+end
+
+User.before_validation do |user|
+  # downcase email of new user
+  if user.new_record?
+
+    user.email.downcase! if user.email.present?
+
+  # do not allow email changes
+  # TODO: keep?
+  elsif user.email_changed?
+
+    user.email = user.email_was
+
+  end
+
+  # only (re)set organisation during creation
+  if user.new_record? || user.org.nil?
+
+    if user.email.present?
+
+      parts_email = user.email.split("@")
+      if parts_email.size == 2
+
+        org_abbr = Rails.application.config.custom["org_domain"][parts_email[1]]
+        org = org_abbr.present? ? Org.find_by_abbreviation(org_abbr) : nil
+        user.org = org.present? ? org : Org.guest
+
+      end
+
+    else
+
+      user.org = Org.guest
+
+    end
+
+  end
+
+  user.ensure_password
+  user.firstname = User.nemo if user.firstname.blank?
+  user.surname = User.nemo if user.surname.blank?
+
+  true
+end
+
+class Org
+
+  def self.guest
+    where(abbreviation: "guests").first
+  end
+
+end
+
+module Users
+
+  class OmniauthCallbacksController
+
+    # rubocop: disable Metrics/MethodLength, Metrics/AbcSize
+    def handle_shibboleth(scheme)
+      auth = request.env["omniauth.auth"]
+
+      # find user by existing identifier
+      user = User.from_omniauth(auth)
+
+      # If the user isn't logged in
+      if current_user.nil?
+
+        # no user found: two reasons:
+        #   1) no user in table users
+        #   2) no identifier of scheme shibboleth yet
+
+        if user.nil?
+          user = User.find_by_email(auth.uid)
+        end
+
+        # still no user: create one
+        if user.nil?
+
+          email = auth["extra"].try("raw_info").try("mail")
+          user = User.new(email: email.downcase)
+          user.surname = auth["extra"].try("raw_info").try("sn")
+          user.firstname = auth["extra"].try("raw_info").try("givenname")
+
+          unless user.save
+
+            flash[:alert] = user.errors.full_messages
+            redirect_to root_path
+            return
+
+          end
+
+        end
+
+        # attach shibboleth identifiers for future use
+        if user.identifiers
+                .select { |id| id.identifier_scheme_id == scheme.id }
+                .empty?
+
+          if Identifier.create(identifier_scheme: scheme,
+                               value: auth.uid,
+                               attrs: auth,
+                               identifiable: user)
+
+            flash[:notice] = _("Your account has been successfully linked to %{scheme}.") % {
+              scheme: scheme.description
+            }
+
+          else
+
+            flash[:alert] = _("Unable to link your account to %{scheme}.") % {
+              scheme: scheme.description
+            }
+
+          end
+
+        end
+
+        sign_in(user)
+
+      # The user is already logged in and just registering the uid with us
+      # If the user could not be found by that uid then attach it to their record
+      elsif user.nil?
+
+        if Identifier.create(identifier_scheme: scheme,
+                             value: auth.uid,
+                             attrs: auth,
+                             identifiable: current_user)
+          flash[:notice] = _("Your account has been successfully linked to %{scheme}.") % {
+            scheme: scheme.description
+          }
+
+        else
+
+          flash[:alert] = _("Unable to link your account to %{scheme}.") % {
+            scheme: scheme.description
+          }
+
+        end
+
+      # If a user was found but does NOT match the current user then the identifier has
+      # already been attached to another account (likely the user has 2 accounts)
+      elsif user.id != current_user.id
+
+        flash[:alert] = _("The current #{scheme.description} iD has been already linked to a user with email #{identifier.user.email}")
+
+      end
+
+      # Redirect to the User Profile page
+      redirect_to edit_user_registration_path
+    end
+    # rubocop: enable Metrics/MethodLength, Metrics/AbcSize
+
+    # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    def handle_orcid(scheme)
+      auth = request.env["omniauth.auth"]
+
+      Rails.logger.info("auth: #{auth}")
+
+      # when saved, identifier of scheme "orcid" is prefixed with the identifier_prefix of the corresponding scheme
+      full_uid = scheme.identifier_prefix + auth.uid
+
+      # The user is already logged in and just registering the uid with us
+      # Action: attach id and redirect to profile page
+      if current_user.present?
+
+        Rails.logger.info("found current_user")
+
+        existing_id = current_user.identifiers
+                                  .select { |id| id.value == full_uid && id.identifier_scheme_id == scheme.id }
+                                  .first
+
+        if existing_id.nil?
+
+          if Identifier.create(identifier_scheme: scheme,
+                               value: auth.uid,
+                               attrs: auth,
+                               identifiable: current_user)
+            flash[:notice] = _("Your account has been successfully linked to %{scheme}.") % {
+              scheme: scheme.description
+            }
+
+          else
+
+            flash[:alert] = _("Unable to link your account to %{scheme}.") % {
+              scheme: scheme.description
+            }
+
+          end
+
+        else
+
+          flash[:alert] = _("Your account has already been linked to %{scheme}") % {
+            scheme: scheme.description
+          }
+
+        end
+
+        redirect_to edit_user_registration_path
+        return
+
+      end
+
+      # User is not logged in
+
+      # Match orcid with one of more users
+      selectable_users = Identifier.where(identifiable_type: "User", identifier_scheme_id: scheme.id, value: full_uid)
+                                   .map(&:identifiable)
+
+      # TODO: create controller
+      if selectable_users.size > 1
+
+        session[:selectable_user_ids] = selectable_users.map(&:id)
+        redirect_to edit_selectable_user_path
+        return
+
+      end
+
+      Rails.logger.info("selectable_users: #{selectable_users.map(&:attributes)}")
+
+      email = auth["info"]["email"]
+
+      user = selectable_users.first
+
+      # Match on ORCID: OK
+      if user
+
+        # set firstname and surname when not present yet
+        user.firstname = auth["info"]["first_name"] if user.firstname.blank? || user.firstname == User.nemo
+        user.surname = auth["info"]["last_name"] if user.surname.blank? || user.surname == User.nemo
+
+      # Match on primary email: OK
+      # this user's orcid must be empty or different
+      # attribute 'email' is unique (enforced by devise?)
+      elsif email.present? && (user = User.where(email: email).first)
+
+        existing_id = user.identifiers
+                          .select { |id| id.value == full_uid && id.identifier_scheme_id == scheme.id }
+                          .first
+
+        if existing_id.nil?
+
+          if Identifier.create(identifier_scheme: scheme,
+                               value: auth.uid,
+                               attrs: auth,
+                               identifiable: user)
+
+            flash[:notice] = _("Your account has been successfully linked to %{scheme}.") % {
+              scheme: scheme.description
+            }
+
+          else
+
+            flash[:alert] = _("Unable to link your account to %{scheme}.") % {
+              scheme: scheme.description
+            }
+
+          end
+
+        end
+
+      # Match on primary email: false
+      # NEW USER. We trust "email" because ORCID marks it as confirmed
+      elsif email.present?
+
+        user = User.new(
+          email: email,
+          firstname: auth["info"]["first_name"],
+          surname: auth["info"]["last_name"]
+        )
+
+        unless user.save
+
+          flash[:alert] = user.errors.full_messages
+          redirect_to root_url
+
+        end
+
+        if Identifier.create(identifier_scheme: scheme,
+                             value: auth.uid,
+                             attrs: auth,
+                             identifiable: user)
+
+          flash[:notice] = _("Your account has been successfully linked to %{scheme}.") % {
+            scheme: scheme.description
+          }
+
+        else
+
+          flash[:alert] = _("Unable to link your account to %{scheme}.") % {
+            scheme: scheme.description
+          }
+
+        end
+
+      # No orcid, no email: warn user
+      else
+
+        flash[:alert] = "Unable to login with orcid: try setting the visibility of your email address to \"everyone\" or \"trusted parties\" (<a href=\"https://orcid.org/account\">orcid profile</a>)"
+        redirect_to root_url
+        return
+
+      end
+
+      set_flash_message(:notice, :success, kind: scheme.description) if is_navigational_format?
+      sign_in_and_redirect user, event: :authentication
+    end
+    # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+    def handle_omniauth(scheme)
+      if scheme.name == "shibboleth"
+        handle_shibboleth(scheme)
+      elsif scheme.name == "orcid"
+        handle_orcid(scheme)
+      end
+    end
+
+  end
+
+end
+
+DMPRoadmap::Application.class_eval do
+  config.custom = config_for(:custom) if File.exist?(Rails.root.join("config", "custom.yml"))
 end
