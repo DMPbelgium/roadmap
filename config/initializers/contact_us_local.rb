bb35b97bca38d0ecbfba906cd0b7c3a7de7c7cb6
root <root@debian-BULLSEYE-live-builder-AMD64>
2024-05-22 09:08:33 +0200

    Fixes mailing issue in contactUs page
    Signed-off-by: root <root@debian-BULLSEYE-live-builder-AMD64>

--- config/initializers/contact_us_local.rb
+++ config/initializers/contact_us_local.rb
@@ -3,8 +3,8 @@
 # See base configuration file at config/initializers/contact_us.rb
 # for more information
 ContactUs.setup do |config|
-  config.mailer_from = "info@dmponline.be"
-  config.mailer_to = "info@dmponline.be"
+  config.mailer_from = "dmponline@belnet.be"
+  config.mailer_to = "servicedesk@belnet.be"
   config.require_name = true
   config.require_subject = true
   config.localize_routes = false
